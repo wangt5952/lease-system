6e0771a40e6d45aae9fb26c2c09613f1eb1c42bc
wangtao <17611789@qq.com>
2018-01-16 17:10:14 +0800

    v0.0.1 增加Redis工具类

---
+++ manager/lease-manager/src/main/java/com/elextec/framework/utils/WzStrUtils.java
@@ -0,0 +1,9 @@
+package com.elextec.framework.utils;
+
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * Created by wangtao on 2018/1/16.
+ */
+public class WzStrUtils extends StringUtils {
+}
