c1b5d4c9f343eada737b6c3fe6e05aa3c4cb6e7f
lumin824 <lumin824@163.com>
2018-01-17 17:15:55 +0800

    no message

--- front-mobile/src/router/index.js
+++ front-mobile/src/router/index.js
@@ -5,11 +5,9 @@ import HelloWorld from '@/components/HelloWorld';
 Vue.use(Router);
 
 export default new Router({
+  mode: 'history',
   routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld,
-    },
+    { path: '/', name: 'HelloWorld', component: HelloWorld },
+    { path: '/login', component: () => import('@/components/Login') },
   ],
 });
--- front-pc/src/main.js
+++ front-pc/src/main.js
@@ -1,6 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
+import Vuex from 'vuex';
 import Resource from 'vue-resource';
 
 import ElementUI from 'element-ui';
@@ -10,7 +11,7 @@ import 'element-ui/lib/theme-chalk/index.css';
 import App from './App';
 import router from './router';
 
-
+Vue.use(Vuex);
 Vue.use(Resource);
 Vue.use(ElementUI);
 
