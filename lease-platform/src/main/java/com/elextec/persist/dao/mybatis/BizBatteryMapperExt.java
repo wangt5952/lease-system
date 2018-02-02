5aaa357d13073e7f323709813c14680c6482c443
w276983305 <33692198+w276983305@users.noreply.github.com>
2018-02-02 10:32:46 +0800

    1

--- lease-platform/src/main/java/com/elextec/persist/dao/mybatis/BizBatteryMapperExt.java
+++ lease-platform/src/main/java/com/elextec/persist/dao/mybatis/BizBatteryMapperExt.java
@@ -1,4 +1,11 @@
 package com.elextec.persist.dao.mybatis;
 
+import com.elextec.persist.model.mybatis.BizBattery;
+import com.elextec.persist.model.mybatis.ext.BizBatteryExt;
+
+import java.util.List;
+
 public interface BizBatteryMapperExt extends BizBatteryMapper {
+    //根据车辆ID获取电池信息
+    public List<BizBatteryExt> getBatteryInfoByVehicleId(String id);
 }
