a571f333fbd3912397d4a83b36489747ee078df7
ykq <947950463@qq.com>
2018-03-08 11:17:55 +0800

    修改重置密码页面

--- front-pc/src/components/ResetPassword.vue
+++ front-pc/src/components/ResetPassword.vue
@@ -5,9 +5,11 @@
       <div class="header-font">
         <div style="font-size:35px;color:#4377B8;">logo</div>
         &nbsp;
-        <div style="margin-top:4px;font-size:23px;color:#F2F2F2">小哥乐途</div>
+        <div style="margin-top:4px;font-size:23px;color:#F2F2F2;width:150px;height:50px">小哥乐途</div>
         <div class="longinButton">
-          <el-button type="info" size="medium">登录</el-button>
+          <el-button type="info" size="medium">
+            <router-link to="login">登录</router-link>            
+          </el-button>
         </div>
         <div class="registerButton"><a href="javascript:void(0)">注册</a></div>
       </div>
@@ -19,21 +21,21 @@
         <div class="resPw-body-left"></div>
         <div class="resPw-body-center">  
           <div style="flex:1;border-bottom:solid 1.5px #EBEBEB;">
-            <div style="background:url(/static/half-ring-15.png);width:45px;height:67px;position: absolute;left:42%;top:90px"></div>
+            <div style="background-image:url(/static/half-ring-15.png);width:45px;height:67px;position: absolute;left:42%;top:90px"></div>
             <div style="font-size:33px;width:180px;margin:3% 40% 0px 45% ">忘记密码</div>
           </div>
           
           <div style="flex:3;display:flex;flex-direction:column;">
-            <div style="background:url(/static/half-ring-13.png);
+            <div style="background-image:url(/static/half-ring-13.png);
                 width:470px;height:40px;margin:20px 350px">
             </div>
             <div style="width:200px;height:20px;margin:0px 450px 10px 430px;font-weight:bold">请填写您需要找回的账号:</div>
 
-            <el-form :model="form">
+            <el-form>
               <div style="width:410px;height:50px;margin:10px 430px 10px 430px">
                 <el-form-item>
-                  <el-input v-model="form.userMobile" style="width: 300px; height: 35px;" placeholder="请输入您的手机号" clearable></el-input>
-                  <el-button style="width: 90px; height: 35px;" type="primary" :disabled="state">获取验证码</el-button>
+                  <el-input v-model.number="sMs.mobile" style="width: 300px; height: 35px;" placeholder="请输入您的手机号" clearable></el-input>
+                  <el-button style="width: 90px; height: 35px;" @click="validateMobileNumber" type="primary" :disabled="state">获取验证码</el-button>
                 </el-form-item>
               </div>
               <div style="width:300px;height:50px;margin:10px 430px 10px 430px">
@@ -44,7 +46,7 @@
             </el-form>
               <div class="nextButton" style="width: 100px; height: 35px;margin:10px 430px 10px 430px">
                 <el-button style="width: 100px; height: 35px;" 
-                    type="primary" @click="validateMobileNumber">下一步</el-button>
+                    type="primary" @click="nextQuery">下一步</el-button>
               </div>
           </div>
         </div>
@@ -57,30 +59,30 @@
         <div class="resPw-body-left"></div>
         <div class="resPw-body-center">  
           <div style="flex:1;border-bottom:solid 1.5px #EBEBEB;">
-            <div style="background:url(/static/half-ring-15.png);width:45px;height:67px;position: absolute;left:42%;top:90px"></div>
+            <div style="background-image:url(/static/half-ring-15.png);width:45px;height:67px;position: absolute;left:42%;top:90px"></div>
             <div style="font-size:33px;width:180px;margin:3% 40% 0px 45% ">忘记密码</div>
           </div>
           
           <div style="flex:3;display:flex;flex-direction:column;">
-            <div style="background:url(/static/half-ring-14.png);
+            <div style="background-image:url(/static/half-ring-14.png);
                 width:470px;height:40px;margin:20px 350px">
             </div>
             <div style="width:200px;height:20px;margin:0px 450px 10px 430px;font-weight:bold">请重置您的密码:</div>
             <el-form :model="form">
               <div style="width:300px;height:50px;margin:10px 430px 10px 430px">
                 <el-form-item>
-                  <el-input v-model="form.newPassword" placeholder="请输入新密码" clearable></el-input>
+                  <el-input v-model="form.newPassword" type="password" placeholder="请输入新密码" clearable></el-input>
                 </el-form-item>
               </div>
               <div style="width:300px;height:50px;margin:10px 430px 10px 430px">
                 <el-form-item>
-                  <el-input v-model="confirmNewPassword" placeholder="请再次输入新密码" clearable></el-input>
+                  <el-input v-model="confirmNewPassword" type="password" placeholder="请再次输入新密码" clearable></el-input>
                 </el-form-item>
               </div>
             </el-form>
             <div class="nextButton" style="width: 300px; height: 35px;margin:10px 430px 10px 430px;">
-              <el-button style="width: 100px; height: 35px;" type="primary">完成</el-button>
-              <el-button style="width: 100px; height: 35px;margin-left:95px" type="info">取消</el-button>
+              <el-button style="width: 100px; height: 35px;" type="primary" @click="confirm">完成</el-button>
+              <el-button style="width: 100px; height: 35px;margin-left:95px" type="info" @click="cancel">取消</el-button>
             </div>
           </div>
         </div>
@@ -107,12 +109,17 @@
 </div>  
 </template>
 <script>
+import md5 from 'js-md5';
 
 export default {
   data(){
     return{
-      
-      form:{},
+      sMs:{
+        needCaptchaToken:false
+      },
+      form:{
+        smsToken:''
+      },
       confirmNewPassword:'',
       stepNo1:true,
       stepNo2:false,
@@ -123,8 +130,53 @@ export default {
   methods:{
     //验证手机号码
     async validateMobileNumber(){
-      let data = await this.$http.post('/api/manager/auth/resetPassword',form);
-      console.log(data);
+      this.state = true;
+      const {...sMs} = this.sMs;
+      try{
+        const { code, message, respData } = (await this.$http.post('/api/manager/auth/sendsms',sMs)).body;        
+        if( code !== '200') throw new Error(message || code);
+        this.$message.success({ message: '验证码发送中，请稍等片刻...', });
+        const { key_sms_vcode_token } = respData;
+        this.form.smsToken = key_sms_vcode_token;
+        // console.log(this.form.smsToken);
+        console.log('最后步骤');
+      } catch (e){
+        const message = e.statusText || e.message;
+        this.$message.error(message);
+      }
+      setTimeout(() =>{
+        this.state = false;
+      }, 120 * 1000 );
+    },
+    // 下一步按钮
+    nextQuery(){
+      this.stepNo1 = false;
+      this.stepNo2 = true;
+    },
+    // 完成按钮
+    async confirm(){
+      const{ smsVCode, smsToken, newPassword } = this.form;
+      if(newPassword === this.confirmNewPassword){
+        const form = { newPassword:md5(newPassword).toUpperCase(),smsVCode,smsToken };
+        try{
+          const{code,message,respData} = (await this.$http.post('/api/manager/auth/resetpassword',form)).body;
+          if(code !== '200') throw new Error(message || code);
+          this.$message.success({
+            message: '重置密码成功！',
+          });
+        } catch (e){
+          const message = e.statusText || e.message;
+          this.$message.error(message);
+        }
+      }else{
+        this.$message.error('两次密码输入不配备,请重新输入');
+        this.cancel();
+      }
+    },
+    // 取消按钮
+    cancel(){
+      this.form.newPassword = '';
+      this.confirmNewPassword = '';
     }
   },
 }
@@ -144,7 +196,7 @@ export default {
   width: 60px; height: 13px; 
 }
 .longinButton{
-  margin:5px 0px 0px 74%
+  margin:5px 0px 0px 70%
 }
 .registerButton{
   font-size:15px;color:white;margin: 12px 0px 0px 30px;
