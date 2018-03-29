8fb15388f9d81c0fa3e019f2305319cc597d5f68
ykq <947950463@qq.com>
2018-03-29 09:39:50 +0800

    修改申请页面

--- front-pc/src/components/Apply.vue
+++ front-pc/src/components/Apply.vue
@@ -1,6 +1,13 @@
 <template>
   <div style="padding:10px;">
-    <h1>审核页面</h1>
+    <template>
+      <el-tabs v-model="activeName" @tab-click="handleClick">
+        <el-tab-pane label="选项卡1" name="first">内容1</el-tab-pane>
+        <el-tab-pane label="选项卡2" name="second">内容2</el-tab-pane>
+        <el-tab-pane label="选项卡3" name="third">内容3</el-tab-pane>
+        <el-tab-pane label="选项卡4" name="fourth">内容4</el-tab-pane>
+      </el-tabs>
+    </template>
   </div>
 </template>
 
@@ -8,8 +15,15 @@
 export default {
   data() {
     return {
-
+      activeName: 'second',
     };
+  },
+  methods: {
+    handleClick() {
+    },
+    // handleClick(tab, event) {
+    //   console.log(tab, event);
+    // },
   },
 };
 </script>
