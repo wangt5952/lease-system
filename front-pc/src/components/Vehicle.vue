0724cf61a34910cc68240efbc3bed3d7c8086d01
ykq <947950463@qq.com>
2018-03-29 09:50:45 +0800

    。

--- front-pc/src/components/Vehicle.vue
+++ front-pc/src/components/Vehicle.vue
@@ -30,7 +30,7 @@
     </div>
     <!-- {{ res }} -->
     <!-- {{ key_user_info }} -->
-    <el-table :data="list"  style="width: 100%;height:85%">
+    <el-table :data="list" height="85%" style="width: 100%">
       <el-table-column prop="vehicleCode" label="编号"></el-table-column>
       <el-table-column prop="vehiclePn" label="型号"></el-table-column>
       <el-table-column prop="vehicleBrand" label="品牌"></el-table-column>
