09108be98c963b8e9791b443fc05f09524c89c4a
ykq <947950463@qq.com>
2018-04-19 09:48:20 +0800

    修改定位点

--- front-pc/src/components/Monitor.vue
+++ front-pc/src/components/Monitor.vue
@@ -490,14 +490,14 @@ export default {
       return new Promise((resolve, reject) => (new BMap.Geolocation()).getCurrentPosition((r) => {
         if (this.getStatus() === BMAP_STATUS_SUCCESS) {
           var mk = new BMap.Marker(r.point);
-          map.addOverlay(mk);
-          map.panTo(r.point);
+          // map.addOverlay(mk);
+          this.mapCenter = r.point;
           alert(`您的位置：${r.point.lng} ,${r.point.lat}`);
         }
         else {
           alert('failed' + this.getStatus());
         }
-      },{enableHighAccuracy: true}));
+      }, { enableHighAccuracy: true }));
     },
   },
   async mounted() {
