fa56e2a9967d747f96b53e4a8d82ffb88aa8a84f
ykq <947950463@qq.com>
2018-06-29 20:48:01 +0800

    去掉${} 语法

--- front-mobile/src/components/Profile.vue
+++ front-mobile/src/components/Profile.vue
@@ -13,7 +13,7 @@
       </cell>
       <cell title="用户类型" :value="this.u_type.value"></cell>
       <cell title="用户" :value="this.key_user_info.loginName"></cell>
-      <cell title="昵称" :value="this.key_user_info.nickName" :link="`/nickName/${key_user_info.nickName}`"></cell>
+      <cell title="昵称" :value="this.key_user_info.nickName" :link="'/nickName/'+ key_user_info.nickName"></cell>
       <cell title="身份证号" :value="this.key_user_info.userPid"></cell>
       <cell title="手机号" :value="this.key_user_info.userMobile"></cell>
       <cell title="所属企业" :value="this.key_user_info.orgName"></cell>
