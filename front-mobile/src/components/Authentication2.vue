9f79927c859dfc0618b99ffebc28a8aa3ba237be
leebighammer <175227974@qq.com>
2018-07-01 15:38:34 +0800

    1

--- front-mobile/src/components/Authentication2.vue
+++ front-mobile/src/components/Authentication2.vue
@@ -202,7 +202,7 @@ export default {
         this.$vux.toast.show({ text: message, type: 'cancel', width: '10em' });
       } else {
         this.$vux.toast.show({ text: '资料提交成功！', type: 'success', width: '10em' });
-        this.$router.replace('/authentication_step3');
+        self.$router.replace('/authentication_step3');
       }
     },
   },
