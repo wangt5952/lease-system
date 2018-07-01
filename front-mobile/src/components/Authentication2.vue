2bd42ff5b15764a4117e7aabad28dd8eaf9456c3
leebighammer <175227974@qq.com>
2018-07-01 16:39:39 +0800

    1

--- front-mobile/src/components/Authentication2.vue
+++ front-mobile/src/components/Authentication2.vue
@@ -195,16 +195,15 @@ export default {
       context.drawImage(sourceCanvas, 0, 0, width, height);
       return canvas;
     },
-    handler() {
-      // const { code, message } = (await this.$http.post('/api/mobile/v1/auth/userrealnameauth',
-      //   { id: this.key_user_info.id, userPid: this.$route.params.id, userIcFront: this.path, userIcBack: this.path1, userIcGroup: this.path2, updateUser: this.key_user_info.loginName })).body;
-      // if (code !== '200') {
-      //   this.$vux.toast.show({ text: message, type: 'cancel', width: '10em' });
-      // } else {
-      //   this.$vux.toast.show({ text: '资料提交成功！', type: 'success', width: '10em' });
-      //   this.$router.push('/authentication_step3');
-      // }
-      this.$router.push('/authentication_step3');
+    async handler() {
+      const { code, message } = (await this.$http.post('/api/mobile/v1/auth/userrealnameauth',
+        { id: this.key_user_info.id, userPid: this.$route.params.id, userIcFront: this.path, userIcBack: this.path1, userIcGroup: this.path2, updateUser: this.key_user_info.loginName })).body;
+      if (code !== '200') {
+        this.$vux.toast.show({ text: message, type: 'cancel', width: '10em' });
+      } else {
+        this.$vux.toast.show({ text: '资料提交成功！', type: 'success', width: '10em' });
+        this.$router.push('/authentication_step3');
+      }
     },
   },
 };
