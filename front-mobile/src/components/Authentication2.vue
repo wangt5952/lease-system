db68d550ce977b79f87de7e230a8a75420a125ca
leebighammer <175227974@qq.com>
2018-07-02 11:53:50 +0800

    1

--- front-mobile/src/components/Authentication2.vue
+++ front-mobile/src/components/Authentication2.vue
@@ -195,9 +195,9 @@ export default {
       return canvas;
     },
     async handler() {
-      // const { code, message } = (await this.$http.post('/api/mobile/v1/auth/userrealnameauth',
-      //   { id: this.key_user_info.id, userPid: this.$route.params.id, userIcFront: this.path, userIcBack: this.path1, userIcGroup: this.path2, updateUser: this.key_user_info.loginName })).body;
-      // if (code !== '200') throw new Error(message || code);
+      const { code, message } = (await this.$http.post('/api/mobile/v1/auth/userrealnameauth',
+        { id: this.key_user_info.id, userPid: this.$route.params.id, userIcFront: this.path, userIcBack: this.path1, userIcGroup: this.path2, updateUser: this.key_user_info.loginName })).body;
+      if (code !== '200') throw new Error(message || code);
       this.$router.push('/authentication_step3');
     },
   },
